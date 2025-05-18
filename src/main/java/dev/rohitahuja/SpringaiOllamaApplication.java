5f099d059d3d87a5fe826569a37e006d9c56968f
Rohit Ahuja <rohit23ahuja@gmail.com>
2025-05-18 21:30:40 +0530

    initial commit

---
+++ src/main/java/dev/rohitahuja/SpringaiOllamaApplication.java
@@ -0,0 +1,13 @@
+package dev.rohitahuja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiOllamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiOllamaApplication.class, args);
+	}
+
+}
